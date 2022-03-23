529e77ed2d00b303a3552419fe452002b44188ba
dbrow206 <dbrow206@uncc.edu>
2022-03-22 21:59:33 -0400

    Updated files

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+//var server = require('./server/app.py')
 //create The App
 var app = express();
 
@@ -13,6 +12,21 @@ let port = 3000;
 let host = 'localhost';
 app.set('view engine', 'ejs');
 
+
+
+const {Client} = require('pg')
+const client = new Client({
+  user:"postgres",
+  password:"postgres",
+  host:"hostname",
+  port: 5432,
+  database:"TransitData"
+})
+
+client.connect()
+.then(() => console.log("Connected to posgres successfully"))
+.catch(e =>console.log)
+.finally(() => client.end())
 
 app.listen(port,  host, ()=>{
   console.log("Server is running on port", port);
@@ -46,6 +60,8 @@ app.get('/stopTrends', (req,res)=>{
 app.get('/suggestions', (req,res)=>{
   res.render('suggestions');
 });
+
+
 
 
 module.exports = app;
