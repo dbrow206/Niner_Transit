1c81bdfb89328aa8e9209b3aa011d4fca3e64976
dbrow206 <dbrow206@uncc.edu>
2022-03-24 10:24:53 -0400

    Updated CSS

--- controllers/trendController.js
+++ controllers/trendController.js
@@ -1,11 +1,16 @@
-
+const server = require('./serverController')
 
 exports.line = (req, res)=>{
-
-    res.render('./trends/lineTrends');
+  server.getLines()
+  .then(result=>{
+    var avgRiders = result[0]
+    var peakTime = result[1];
+    res.render('./trends/lineTrends', {avgRiders, peakTime});
+  })
+  .catch();
 };
 
 exports.stop = (req, res, next) => {
-    return res.render('./trends/stopTrends');
+  return res.render('./trends/stopTrends');
 }   
 
